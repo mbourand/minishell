12d0eaf3b5bbd0085424390ec4860a8d082767c5
nforay <nforay@student.42.fr>
2020-07-31 20:28:22 +0200

    Added unset+env

--- srcs/builtin_env.c
+++ srcs/builtin_env.c
@@ -1,27 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*   builtin_env.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nforay <nforay@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/08/29 18:00:37 by mbourand          #+#    #+#             */
-/*   Updated: 2020/07/31 18:43:40 by nforay           ###   ########.fr       */
+/*   Created: 2020/07/28 23:50:00 by nforay            #+#    #+#             */
+/*   Updated: 2020/07/31 20:23:05 by nforay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+# include "minishell.h"
 
-int	ft_strcmp(const char *s1, const char *s2)
+static void env_print(t_env *env)
 {
-	size_t i;
+	ft_printf("%s=%s\n", env->key, env->val);
+}
 
-	i = 0;
-	while (s1[i] && s2[i])
-	{
-		if ((unsigned char)(s1[i]) != (unsigned char)(s2[i]))
-			return ((unsigned char)(s1[i]) - (unsigned char)(s2[i]));
-		i++;
-	}
-	return ((unsigned char)(s1[i]) - (unsigned char)(s2[i]));
+int	btin_env(t_list *env, t_list *command)
+{
+	(void)command;
+	ft_lstiter(env, (void*)&env_print);
+	return (SUCCESS);
 }
---
+++ srcs/builtin_unset.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin_unset.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nforay <nforay@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/28 23:50:21 by nforay            #+#    #+#             */
+/*   Updated: 2020/07/31 20:03:13 by nforay           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "minishell.h"
+
+static void	remove_key(t_list *env)
+{
+	if (DEBUG) ft_printf("\e[31m[DEBUG]\e[39mremoving key:%s val:%s\n", ((t_env*)env)->key, ((t_env*)env)->val);
+	free(((t_env*)env)->key);
+	free(((t_env*)env)->val);
+}
+
+int	btin_unset(t_list *env, t_list *command)
+{
+	t_list *tmp;
+	t_list	*envtmp;
+	size_t	i;
+
+	envtmp = env;
+	i = 0;
+	while (envtmp)
+	{
+		tmp = command->next;
+		while (tmp && envtmp)
+		{
+			if (!(ft_strcmp(((t_token*)tmp->content)->text, ((t_env*)envtmp->content)->key)))
+			{
+				if (DEBUG) ft_printf("\e[31m[DEBUG]\e[39mKey %s found at %d/%d.\n", ((t_token*)tmp->content)->text, i, ft_lstsize(env) - 1);
+				envtmp = envtmp->next;
+				tmp = command->next;
+				ft_lstdelat(&env, i, (void*)&remove_key);
+			}
+			else
+				tmp = tmp->next;
+		}
+		i++;
+		if (envtmp && envtmp->next)
+			envtmp = envtmp->next;
+	}
+	return (SUCCESS);
+}
