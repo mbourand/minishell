9c59b36148a548385a902a974118693a640689e9
mbourand <mbourand@student.42.fr>
2020-08-01 14:17:52 +0200

    Fix norme buggée

--- srcs/tokens.c
+++ srcs/tokens.c
@@ -6,7 +6,7 @@
 /*   By: mbourand <mbourand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/07 18:38:02 by mbourand          #+#    #+#             */
-/*   Updated: 2020/07/07 18:38:02 by mbourand         ###   ########.fr       */
+/*   Updated: 2020/07/31 18:37:06 by mbourand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@
 
 t_token	*create_token(char *text)
 {
-	t_token* token;
+	t_token	*token;
 
 	if (!(token = malloc(sizeof(t_token) * 1)))
 		exit(1);
@@ -33,7 +33,8 @@ t_token	*create_token(char *text)
 /*
 **	Crée un nouveau mot en créant un nouveau token
 */
-void	new_word(t_list **iter, t_token** token)
+
+void	new_word(t_list **iter, t_token **token)
 {
 	ft_lstadd_back(iter, ft_lstnew(create_token(ft_strnew(""))));
 	(*iter) = (*iter)->next;
@@ -43,13 +44,15 @@ void	new_word(t_list **iter, t_token** token)
 /*
 **	Copie de shell->input dans token->text tout ce qui est entre quote
 */
-static void	process_quote(t_token *token, t_shell *shell, size_t *i)
+
+void	prcs_quote(t_token *token, t_shell *shell, size_t *i)
 {
 	char	*tmp;
 
 	ft_straddchar(&(token->text), shell->input[*i], 1);
 	tmp = token->text;
-	token->text = ft_strjoinuntil(token->text, shell->input + *i + 1, shell->input[*i]);
+	token->text = ft_strjoinuntil(token->text, shell->input + *i + 1,
+		shell->input[*i]);
 	ft_free(&tmp);
 	(*i) += ft_strlenuntil(shell->input + *i + 1, shell->input[*i]) + 2;
 	ft_straddchar(&(token->text), shell->input[*i - 1], 1);
@@ -58,7 +61,8 @@ static void	process_quote(t_token *token, t_shell *shell, size_t *i)
 /*
 **	Crée un nouveau mot si il y a d'autres mots après les espaces
 */
-static void process_space(t_token **token, t_shell *shell, size_t *i, t_list **iter)
+
+void	prcs_space(t_token **token, t_shell *shell, size_t *i, t_list **iter)
 {
 	if (shell->input[*i + 1])
 		new_word(iter, token);
@@ -71,7 +75,8 @@ static void process_space(t_token **token, t_shell *shell, size_t *i, t_list **i
 **	Copie l'opérateur dans le nouveau mot
 **	Crée un nouveau mot si il y a d'autres mots après
 */
-static void process_operator(t_token **token, t_shell *shell, size_t *i, t_list **iter)
+
+void	prcs_operator(t_token **token, t_shell *shell, size_t *i, t_list **iter)
 {
 	size_t	op_len;
 	char	*tmp;
@@ -92,7 +97,8 @@ static void process_operator(t_token **token, t_shell *shell, size_t *i, t_list 
 /*
 **	Copie le caractère dans token->text
 */
-static void process_character(t_token *token, t_shell *shell, size_t *i)
+
+void	process_character(t_token *token, t_shell *shell, size_t *i)
 {
 	ft_straddchar(&(token->text), shell->input[*i], 1);
 	(*i)++;
@@ -101,6 +107,7 @@ static void process_character(t_token *token, t_shell *shell, size_t *i)
 /*
 **	Découpe l'input complet shell->input en mots et les place dans shell->tokens
 */
+
 void	get_tokens(t_shell *shell)
 {
 	t_list	*begin;
@@ -117,13 +124,13 @@ void	get_tokens(t_shell *shell)
 	while (shell->input[i])
 	{
 		if (is_quote(shell->input[i]))
-			process_quote(token, shell, &i);
+			prcs_quote(token, shell, &i);
 		else if (is_blank(shell->input[i]))
-			process_space(&token, shell, &i, &iter);
+			prcs_space(&token, shell, &i, &iter);
 		else if (operator_length(shell->input + i) > 0)
-			process_operator(&token, shell, &i, &iter);
+			prcs_operator(&token, shell, &i, &iter);
 		else
-			process_character(token, shell, &i);
+			prcs_character(token, shell, &i);
 	}
 	shell->tokens = begin;
 }
