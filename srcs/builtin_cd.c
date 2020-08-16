5a44927eb352829784db9aaa8918396b7bb2293c
nforay <nforay@student.42.fr>
2020-08-16 15:07:31 +0200

    Reproduced bash expected behavior

--- srcs/builtin_cd.c
+++ srcs/builtin_cd.c
@@ -6,23 +6,43 @@
 /*   By: nforay <nforay@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/11 14:57:45 by nforay            #+#    #+#             */
-/*   Updated: 2020/08/16 01:08:01 by nforay           ###   ########.fr       */
+/*   Updated: 2020/08/16 15:04:34 by nforay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	update_env_pwd(t_list *env, t_shell *shell)
+static void	update_env_pwd(t_list *env, t_shell *shell, t_list *command)
 {
 	char	buf[PATH_MAX];
+	char	*tmp;
+	char	*ptr;
 
-	shell->cwd = getcwd(buf, PATH_MAX);
-	free((get_env(env, "OLDPWD"))->val);
-	(get_env(env, "OLDPWD"))->val = (get_env(env, "PWD"))->val;
-	(get_env(env, "PWD"))->val = ft_strdup(shell->cwd);
+	ptr = (get_env(env, "PWD"))->val;
+	if (!(shell->cwd = getcwd(buf, PATH_MAX)))
+	{ // break me: mkdir -p test1/test2/test3 && cd test1/test2/test3 && rm -r ../../../test1/ && cd ..
+		ft_dprintf(STDERR_FILENO, "cd: error retrieving current directory: ");
+		ft_dprintf(STDERR_FILENO, "getcwd: cannot access parent directories: ");
+		ft_dprintf(STDERR_FILENO, "No such file or directory\n");
+		tmp = ft_strjoin("/", ((t_token*)command->next->content)->text);
+		(get_env(env, "PWD"))->val = ft_strjoin(ptr, tmp);
+		free(ptr);
+		free(tmp);
+	}
+	else
+	{
+		if ((get_env(env, "OLDPWD"))->val)
+		{
+			free((get_env(env, "OLDPWD"))->val);
+			(get_env(env, "OLDPWD"))->val = (get_env(env, "PWD"))->val;
+		}
+		else
+			free((get_env(env, "PWD"))->val);
+		(get_env(env, "PWD"))->val = ft_strdup(shell->cwd);
+	}
 }
 
-static int	changedir_home(t_list *env, t_shell *shell)
+static int	changedir_home(t_list *env, t_shell *shell, t_list *command)
 {
 	//DIR		*folder; //Maybe unnecessary to try open before chdir
 	if (DEBUG) ft_printf("\e[31m[DEBUG]\e[39mchange working dir: %s\n", (get_env(env, "HOME"))->val);
@@ -30,7 +50,7 @@ static int	changedir_home(t_list *env, t_shell *shell)
 		return (FAILURE);*/
 	if (!(chdir((get_env(env, "HOME"))->val)))
 	{
-		update_env_pwd(env, shell);
+		update_env_pwd(env, shell, command);
 		if (DEBUG) ft_printf("\e[31m[DEBUG]\e[39mcurrent working dir: %s\n", shell->cwd);
 		return (SUCCESS);
 	}
@@ -47,7 +67,7 @@ static int	changedir_path(t_list *env, t_shell *shell, t_list *command)
 	if (DEBUG) ft_printf("\e[31m[DEBUG]\e[39mchange working dir: %s\n", ((t_token*)command->next->content)->text);
 	if (!(chdir(((t_token*)command->next->content)->text)))
 	{
-		update_env_pwd(env, shell);
+		update_env_pwd(env, shell, command);
 		if (DEBUG) ft_printf("\e[31m[DEBUG]\e[39mcurrent working dir: %s\n", shell->cwd);
 		return (SUCCESS);
 	}
@@ -65,7 +85,7 @@ int			btin_cd(t_shell *shell, t_list *command)
 
 	env = shell->env;
 	if (!((t_token*)command->next))
-		return (changedir_home(env, shell));
+		return (changedir_home(env, shell, command));
 	else
 		return (changedir_path(env, shell, command));
 }
--- srcs/builtin_pwd.c
+++ srcs/builtin_pwd.c
@@ -6,7 +6,7 @@
 /*   By: nforay <nforay@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/13 20:32:12 by nforay            #+#    #+#             */
-/*   Updated: 2020/08/16 00:59:16 by nforay           ###   ########.fr       */
+/*   Updated: 2020/08/16 14:57:51 by nforay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,11 @@
 
 int	btin_pwd(t_shell *shell, t_list *command)
 {
+	char	buf[PATH_MAX];
+
 	(void)command;
-	if (!(getcwd(shell->cwd, PATH_MAX)))
-	{
-		ft_perror("pwd");
-		ft_putendl_fd("No such file or directory", STDOUT_FILENO);
-	}
+	if (!(shell->cwd = getcwd(buf, PATH_MAX))) // break me: mkdir -p test1/test2/test3 && cd test1/test2/test3 && rm -r ../../../test1/ && cd ..
+		ft_putendl_fd((get_env(shell->env, "PWD"))->val, STDOUT_FILENO);
 	else
 		ft_putendl_fd(shell->cwd, STDOUT_FILENO);
 	return (SUCCESS);
