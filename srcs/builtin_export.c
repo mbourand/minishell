74aa308b8e94f61c2190cab23b84d747add1430e
nforay <nforay@student.42.fr>
2020-08-22 16:23:10 +0200

    Fixed invalid identifier

--- srcs/builtin_export.c
+++ srcs/builtin_export.c
@@ -17,10 +17,47 @@ static void	export_print(t_env *env)
 	ft_printf("declare -x %s=\"%s\"\n", env->key, env->val);
 }
 
+static int	check_export_key(char *key)
+{
+	if (ft_isdigit(*key))
+		return (0);
+	while (*key)
+	{
+		if (ft_isalnum(*key) || *key == '_')
+			key++;
+		else
+			return (0);
+	}
+	return (1);
+}
+
+static int	export_error(t_env *new)
+{
+	ft_printf("minishell: export: `%s=%s': not a valid identifier\n", new->key, new->val);
+	free(new->key);
+	free(new->val);
+	free(new);
+	return (FAILURE);
+}
+
+static void	export_add_or_replace(t_env *new, t_list *env)
+{
+	t_env	*tmp;
+
+	if ((tmp = get_env(env, new->key)) != NULL)
+	{
+		free(tmp->val);
+		tmp->val = new->val;
+		free(new->key);
+		free(new);
+	}
+	else
+		ft_lstadd_back(&env, ft_lstnew(new));
+}
+
 int	btin_export(t_list *command)
 {
 	t_env	*new;
-	t_env	*tmp;
 	t_list	*env;
 
 	env = g_shell.env;
@@ -31,18 +68,8 @@ int	btin_export(t_list *command)
 	}
 	new = parse_env(((t_token*)command->next->content)->text);
 	if (DEBUG) ft_printf("\e[31m[DEBUG]\e[39mkey:%s|value:%s\n", new->key, new->val);
-	if ((tmp = get_env(env, new->key)) != NULL)
-	{
-		free(tmp->val);
-		tmp->val = new->val;
-		free(new->key);
-		free(new);
-		if (DEBUG) ft_printf("\e[31m[DEBUG]\e[39mkey found, changing value.\n");
-	}
-	else
-	{
-		ft_lstadd_back(&env, ft_lstnew(new));
-		if (DEBUG) ft_printf("\e[31m[DEBUG]\e[39mt_env added to env at position %d.\n", ft_lstsize(env) - 1);
-	}
+	if (!(check_export_key(new->key)))
+		return (export_error(new));
+	export_add_or_replace(new, env);
 	return (SUCCESS);
 }
