ef96d68fda6873bb9adb9512efdea3388483f080
nforay <nforay@student.42.fr>
2020-09-11 18:37:16 +0200

    exit 2 on arg error

--- srcs/builtin_exit.c
+++ srcs/builtin_exit.c
@@ -6,7 +6,7 @@
 /*   By: nforay <nforay@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/16 16:41:35 by nforay            #+#    #+#             */
-/*   Updated: 2020/08/16 17:37:20 by nforay           ###   ########.fr       */
+/*   Updated: 2020/09/11 18:35:45 by nforay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,9 +29,12 @@ int	btin_exit(t_list *command)
 		while (str && (ft_isdigit(*str) || *str == 43 || *str == 45))
 			str++;
 		if (*str != 0)
+		{
 			ft_dprintf(STDERR_FILENO,
 			"minishell: exit: %s: numeric argument required\n",
 			((t_token*)command->next->content)->text);
+			exit(2);
+		}
 		else
 			exit(excode);
 	}
